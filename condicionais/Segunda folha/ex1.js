6fa7e62feefc173b6ee0a475a5e746ffe9c05cde
Erick Cardoso <cyber666king@gmail.com>
2022-09-17 08:57:54 -0300

    Finaliza primeiro exericício do enunciado

---
+++ condicionais/Segunda folha/ex1.js
@@ -0,0 +1,8 @@
+/* 1) Faça um programa que receba dois números, calcule e
+mostre a subtração do primeiro pelo segundo. */
+
+let num1 = parseFloat(prompt('Digite um número '))
+let num2 = parseFloat(prompt('Digite outro número '))
+
+let subtracao = num1 - num2
+alert('O resultado da sua subtração é ' + subtracao) 
