dfc9e27f1d9127fd26ce675ef6214c9d00c747e9
Erick Cardoso <cyber666king@gmail.com>
2022-09-17 09:05:56 -0300

    Finaliza segundo exercicio

---
+++ condicionais/Segunda folha/ex2.js
@@ -0,0 +1,9 @@
+/* 2) Faça um programa que receba três números, calcule e
+mostre a multiplicação entre eles. */
+
+let num1 = parseFloat(prompt('Digite um número'))
+let num2 = parseFloat(prompt('Digite um número'))
+let num3 = parseFloat(prompt('Digite um número'))
+
+let multiplica = num1 * num2 * num3
+alert(num1 + ' * ' + num2 + ' * ' + num3 + ' = ' + multiplica)
