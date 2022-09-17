4c91fbe33e9d937ecf5772285be646eb62514958
Erick Cardoso <cyber666king@gmail.com>
2022-09-17 09:44:24 -0300

    Finaliza quinto exercicios

---
+++ condicionais/Segunda folha/ex5.js
@@ -0,0 +1,7 @@
+/* 5) Faça um programa que receba o peso de uma pessoa em
+quilos, calcule e mostre esse peso em gramas. */
+
+let peso = parseFloat(prompt('Digite seu peso '))
+
+let calc = peso * 1000
+alert('Você tem ' + calc + ' gramas')
