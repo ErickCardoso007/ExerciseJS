b9e02192108f469ee657f8ea4c22e33477a03b49
Erick Cardoso <cyber666king@gmail.com>
2022-09-17 09:11:38 -0300

    Finaliza terceiro exercicio

---
+++ condicionais/Segunda folha/ex3.js
@@ -0,0 +1,12 @@
+/* 3) Faça um algoritmo que recebe quatro números inteiros,
+calcule e mostre a soma desses números.
+*/
+
+let num1 = parseInt(prompt('Digite o primeiro número'))
+let num2 = parseInt(prompt('Digite o segundo número'))
+let num3 = parseInt(prompt('Digite o terceiro número'))
+let num4 = parseInt(prompt('Digite o quarto número'))
+
+let soma = num1 + num2 + num3 + num4
+
+alert(num1 + ' + ' + num2 + ' + ' + num3 + ' + ' + num4 + ' = ' + soma)
