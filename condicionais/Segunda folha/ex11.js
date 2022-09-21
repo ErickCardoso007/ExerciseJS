36a8d13786cb1df24fb612c16afb727b5d6b3c01
Erick Cardoso <cyber666king@gmail.com>
2022-09-20 22:54:54 -0300

    Finaliza décimo primeiro exercício de condicionais

---
+++ condicionais/Segunda folha/ex11.js
@@ -0,0 +1,12 @@
+/*
+11)Faça um programa que leia duas informações do usuário,
+primeiro deve ser lido o SOBRENOME e depois o NOME
+de uma pessoa, e após o programa deve exibir a seguinte
+mensagem: “Sr(a). <NOME> <SOBRENOME>, é uma
+honra contar com sua presença!”.
+*/
+
+let lastName = prompt('Enter your last name ')
+let name = prompt('Enter your name ')
+
+alert('Sr(a). ' + name + ' ' + lastName + ', é uma honra contar com sua presença!')
